632f66347d6075ba5e171ba9cd01142ca553910f
geekchanskiy <astrovskiy.04@gmail.com>
2025-11-18 21:23:06 +0300

    drf jwt added

--- project/users/middleware.py
+++ project/users/middleware.py
@@ -1,12 +1,16 @@
 from .models import Profile
 
+from django.contrib.auth.models import AnonymousUser
+
 
 class ProfileMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user is not None:
+        if request.user.is_authenticated:
+            print(type(request.user))
+            print(request.user)
             profile = Profile.objects.get(user__id=request.user.id)
             request.profile = profile
 
