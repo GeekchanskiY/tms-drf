3f649b7f83db90f0e03ad2298e1846f39c3bf883
geekchanskiy <astrovskiy.04@gmail.com>
2025-11-25 21:45:49 +0300

    add auto create user profile signal

--- project/users/models.py
+++ project/users/models.py
@@ -1,10 +1,13 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .utils import get_user_cache_key
+
+User = get_user_model()
 
 
 class Profile(models.Model):
@@ -18,3 +21,9 @@ class Profile(models.Model):
 @receiver(post_save, sender=Profile, dispatch_uid="clear_profile_cache")
 def delete_balance_cache(sender, instance, **kwargs):
     cache.delete(get_user_cache_key(instance.user.id))
+
+
+@receiver(post_save, sender=User)
+def user_created_callback(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance, balance=0)
